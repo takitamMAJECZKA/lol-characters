e619910c86acf85dc9db8cf0adf3fde62c2af6e4
Maciej Majka <magic_of_x@o2.pl>
2025-02-13 22:14:05 +0100

    Gallery is done, made responsible and made some changes in css and related Card to Gallery

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,14 +1,17 @@
 import Gallery from "./Gallery";
-import { useState, useContext, useEffect,} from "react";
+import { useState, useEffect, createContext,} from 'react';
+
+export let characterContext = createContext();
 
 
 export default function Card( {character} ){
-    let [photoNum, setPhotoNum] = useState(1)
-    let [image, setImage] = useState(`./src/assets/${character+photoNum}.jpg`)
+    const paragraphStyles = {WebkitLineClamp: 2, WebkitBoxOrient: 'vertical', overflow: 'hidden', display: '-webkit-box'}
+
+
+    let [image, setImage] = useState(`./src/assets/${character+1}.jpg`)
     let description;
 
     let [pOpened, setPOpened] = useState(false)
-    const paragraphStyles = {WebkitLineClamp: 2, WebkitBoxOrient: 'vertical', overflow: 'hidden', display: '-webkit-box'}
     switch (character){
         case "Akali":
             description = "Opuściwszy Zakon Kinkou i wyzbywszy się tytułu Pięści Cienia, Akali działa w pojedynkę, gotowa stać się śmiercionośną bronią, której jej lud tak bardzo potrzebował. Choć nie wyrzekła się wiedzy, którą przekazał jej mistrz Shen, poprzysięgła zabijać wrogów Ionii jednego po drugim. Akali uderza wprawdzie w niczym niezmąconej ciszy, ale jej przesłanie rozbrzmiewa z wielką mocą: bój się zabójczyni bez mistrza.";
@@ -20,11 +23,16 @@ export default function Card( {character} ){
             description = "Kennen jest kimś więcej niż błyskawicznie szybkim stróżem ioniańskiej równowagi, jest jedynym yordlowym członkiem zakonu Kinkou. Mimo że jest małym i włochatym stworzeniem, chętnie stawi czoła wszystkim zagrożeniom za pomocą wirującej burzy shurikenów i dzięki niekończącemu się entuzjazmowi. U boku swojego mistrza Shena, Kennen patroluje duchowy wymiar, używając niszczącej elektrycznej energii, by zabijać wrogów.";
             break;
     }
+    function getImageFromChild(choosedImage){
+        setImage(`./src/assets/${character+choosedImage}.jpg`)
+    }
     return(<div className="card">
-        <img src={image}/>
+        <img className="profileImg" src={image} />
         <h2>{character}</h2>
         <p style={pOpened ? null : paragraphStyles}>{description}</p>
-        <button className="moreLessBtn" onClick={() => {setPOpened(!pOpened)}}>{pOpened ? "Read less.." : "Read more..."}</button>
-        <Gallery />
+        <button className="moreLessBtn" onClick={() => {setPOpened(!pOpened)}}>{pOpened ? "Read less.." : "Read more..."}</button><br/>
+        <characterContext.Provider value={character}>
+            <Gallery sendChoosedImage={getImageFromChild}/>
+        </characterContext.Provider>
     </div>)
 }
--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -4,13 +4,16 @@ import kennen1 from './assets/Kennen1.jpg'
 import kennen2 from './assets/Kennen2.jpg'
 import neeko1 from './assets/Neeko1.jpg'
 import neeko2 from './assets/Neeko2.jpg'
+import {useContext} from 'react'
+import { characterContext } from './Card'
 
 
-
-export default function Gallery(){
+export default function Gallery( {sendChoosedImage} ){
+    let character = useContext(characterContext)
     
-    return(<>
-<img src={}>
-<img src={}>
-</>)
+
+return(<div className='galleryContainer'>
+    <img src={`./src/assets/${character+1}.jpg`} className='galleryImg' onClick={() => {sendChoosedImage(1)}}/>
+    <img src={`./src/assets/${character+2}.jpg`} className='galleryImg' onClick={() =>{sendChoosedImage(2)}}/>
+</div>)
 }
